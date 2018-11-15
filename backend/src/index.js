fb8b82270cb8250ed6614848542ef2c04cb6ad77
Alex Matseshe <amatseshe@gmail.com>
2018-11-15 16:18:06 -0600

    Add prisma

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,14 @@
-// let's go!
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+server.start({
+  cors: {
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  },
+}, details => {
+  console.log(`Server is running on port http:/localhost:${details.port}`);
+});
