3c478b51489012f134ff98d37586020c8bfcd8f9
Alex Matseshe <amatseshe@gmail.com>
2018-11-15 16:22:11 -0600

    Add primsa

---
+++ backend/src/db.js
@@ -0,0 +1,11 @@
+// This file connects to the remote prisma DB
+const { Prisma } = require('prisma-binding');
+
+const db = new Prisma({
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_SECRET,
+  debug: false,
+});
+
+module.exports = db;
