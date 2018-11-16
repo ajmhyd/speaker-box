b288322a23a1221d5b63288445bb724c52c62965
Alex Matseshe <amatseshe@gmail.com>
2018-11-16 04:01:11 -0600

    Add yoga

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,19 @@
-const Mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if they are logged in
+
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args
+      }
+    }, info);
+
+    return item;
+  }
+  // createDog(parent, args, ctx, info) {
+  //   //create a dog
+  //   console.log(args);
+  // }
+};
 
 module.exports = Mutations;
