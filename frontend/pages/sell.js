f4c417c7775e25a1680a69efcb45570940411189
Alex Matseshe <amatseshe@gmail.com>
2018-11-19 17:12:09 -0600

    Add createItem form

---
+++ frontend/pages/sell.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CreateItem from '../components/CreateItem';
+
+const sell = (props) => {
+  return (
+    <div>
+      <CreateItem />
+    </div>
+  );
+};
+
+export default sell;
