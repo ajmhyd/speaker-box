afca72d26115f7aa68eed6ecc9207b0a35368216
Alex Matseshe <amatseshe@gmail.com>
2018-11-20 15:31:29 -0600

    Add singleItem

---
+++ frontend/components/Pagination.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PaginationStyles from './styles/PaginationStyles';
+
+const Pagination = (props) => {
+  return (
+    <PaginationStyles>
+      <p>pagination!</p>
+    </PaginationStyles>
+  );
+};
+
+export default Pagination;
--- frontend/pages/sell.js
+++ frontend/pages/sell.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import CreateItem from '../components/CreateItem';
 
 const Sell = (props) => {
