77b5316dd66696403fdb237c67321794e48c87e5
Alex Matseshe <amatseshe@gmail.com>
2018-11-19 18:22:21 -0600

    Add image upload to createItem

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -42,6 +42,23 @@ class CreateItem extends Component {
     const val = type === 'number' ? parseFloat(value) : value;
     this.setState({ [name]: value });
   }
+
+  uploadFile = async (e) => {
+    const files = e.target.files;
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('upload_preset', 'sickfits');
+
+    const res = await fetch('	https://api.cloudinary.com/v1_1/amatseshe/image/upload', {
+      method: 'POST',
+      body: data,
+    });
+    const file = await res.json();
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url,
+    })
+  }
   render() {
     return (
       <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state} >
@@ -59,6 +76,18 @@ class CreateItem extends Component {
           }}>
           <Error error={error} />
             <fieldset disabled={loading} aria-busy={loading}>
+            <label htmlFor="file">
+                Image
+                <input
+                  type="file"
+                  id="file"
+                  name="file"
+                  placeholder="Upload an image"
+                  required
+                  onChange={this.uploadFile} />
+                  {this.state.image && <img width="200" src={this.state.image} alt="Image preview" />}
+              </label>
+
               <label htmlFor="title">
                 Title
                 <input
