c66e7756cf29d911a9f0d392a27f4211d9aae148
Alex Matseshe <amatseshe@gmail.com>
2018-11-20 16:31:06 -0600

    Add pagination

--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -3,6 +3,7 @@ import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import styled from 'styled-components';
 import Item from './Item';
+import Pagination from './Pagination';
 
 const ALL_ITEMS_QUERY = gql`
   query ALL_ITEMS_QUERY {
@@ -32,15 +33,17 @@ class Items extends Component {
   render() {
     return (
       <Center>
-        <Query query={ALL_ITEMS_QUERY}>
-          {({ data, error, loading }) => {
-            if(loading) return <p>Loading...</p>;
-            if(error) return <p>Error: {error.message}</p>;
-            return <ItemsList>
-              {data.items.map(item => <Item item={item} key={item.id}/>)}
-            </ItemsList>;
-          }}
-        </Query>
+        <Pagination page={this.props.page} />
+          <Query query={ALL_ITEMS_QUERY}>
+            {({ data, error, loading }) => {
+              if(loading) return <p>Loading...</p>;
+              if(error) return <p>Error: {error.message}</p>;
+              return <ItemsList>
+                {data.items.map(item => <Item item={item} key={item.id}/>)}
+              </ItemsList>;
+            }}
+          </Query>
+        <Pagination page={this.props.page} />
       </Center>
     );
   }
