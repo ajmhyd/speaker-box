b5cb2ef21771bfe999aaf756661a0d034a32cd94
Alex Matseshe <amatseshe@gmail.com>
2018-11-20 06:03:17 -0600

    Add DeleteItem component

--- frontend/components/Item.js
+++ frontend/components/Item.js
@@ -5,6 +5,7 @@ import ItemStyles from './styles/ItemStyles';
 import PriceTag from './styles/PriceTag';
 import Link from 'next/link';
 import formatMoney from '../lib/formatMoney';
+import DeleteItem from './DeleteItem';
 
 class Item extends Component {
   render() {
@@ -31,7 +32,7 @@ class Item extends Component {
             <a>Edit</a>
           </Link>
           <button>Add To Cart</button>
-          <button>Delete</button>
+          <DeleteItem id={item.id}>Delete This Item</DeleteItem>
         </div>
       </ItemStyles>
     );
