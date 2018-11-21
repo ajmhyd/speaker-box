bac198068be8628720fa83c07fe6217155f4f034
Alex Matseshe <amatseshe@gmail.com>
2018-11-21 10:10:45 -0600

    Add user

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
@@ -8,7 +9,17 @@ const server = createServer();
 
 server.express.use(cookieParser());
 // TODO: use express middleware to populate current user
+// decode the jwt to get the user id on each request
 
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if(token) {
+    const {userId} = jwt.verify(token, process.env.APP_SECRET);
+    // put userIUd onto the req for future request to access
+    req.userId = userId;
+  }
+  next();
+})
 server.start({
   cors: {
     credentials: true,
