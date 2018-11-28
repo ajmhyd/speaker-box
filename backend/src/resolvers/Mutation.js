918d468e3413e89c22ec3432ef9cd812673e4bb4
Alex Matseshe <amatseshe@gmail.com>
2018-11-28 16:33:47 -0600

    Add saving orders to db

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -4,6 +4,7 @@ const { randomBytes } = require('crypto');
 const { promisify } = require('util');
 const { transport, niceEmail } = require('../mail');
 const { hasPermission } = require('../utils');
+const stripe = require('../stripe');
 
 const Mutations = {
   async createItem(parent, args, ctx, info) {
@@ -246,6 +247,58 @@ const Mutations = {
     return ctx.db.mutation.deleteCartItem({
       where: { id: args.id },
     }, info);
+  },
+  async createOrder(parent, args, ctx, info) {
+    // 1. query current user and make sure they are signed in
+    const { userId } = ctx. request;
+    if(!userId) throw new Error('You must be signed in to complete this order.');
+    const user = await ctx.db.query.user({where: { id: userId } },
+      `{
+        id
+        name
+        email
+        cart {
+          id
+          quantity
+          item { title price id description image largeImage }
+        }}`
+    );
+    // 2. recalculate the total for price
+    const amount = user.cart.reduce((tally, cartItem) => tally + cartItem.item.price * cartItem.quantity, 0);
+    // 3. create the stripe charge
+    const charge = await stripe.charges.create({
+      amount,
+      currency: 'USD',
+      source: args.token,
+    });
+    // 4. convert the cartItems to OrderItems
+    const orderItems = user.cart.map(cartItem => {
+      const orderItem ={
+        ...cartItem.item,
+        quantity: cartItem.quantity,
+        user: { connect: { id: userId } },
+      };
+      delete orderItem.id;
+      return orderItem;
+    });
+    // 5. create the order
+    const order = await ctx.db.mutation.createOrder({
+      data: {
+        total: charge.amount,
+        charge: charge.id,
+        items: { create: orderItems },
+        user: { connect: { id: userId } },
+      },
+    });
+    // 6. clean up - clear the users cart, delete cartItems
+    const cartItemIds = user.cart.map(cartItem => cartItem.id);
+    await ctx.db.mutation.deleteManyCartItems({
+      where: {
+      id_in: cartItemIds,
+      },
+    });
+    // 7. return the order to the client
+    return order;
   },
 };
 
--- frontend/components/TakeMyMoney.js
+++ frontend/components/TakeMyMoney.js
@@ -9,28 +9,55 @@ import calcTotalPrice from '../lib/calcTotalPrice';
 import Error from './ErrorMessage';
 import User, { CURRENT_USER_QUERY } from './User';
 
+const CREATE_ORDER_MUTATION = gql`
+  mutation createOrder($token: String!) {
+    createOrder(token: $token) {
+      id
+      charge
+      totalItems
+      items {
+        id
+        title
+      }
+    }
+  }
+`;
+
 function totalItems(cart) {
   return cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0);
-}
+};
 
-onToken = (res) => {
-  console.log(res.id);
-}
+onToken = async (res, createOrder) => {
+  // manually call mutation once we have the stripe token
+  const order = await createOrder({
+    variables: {
+      token: res.id,
+    },
+  }).catch(err => {
+    alert(err.message);
+  });
+};
+
 class TakeMyMoney extends React.Component {
   render() {
     return (
       <User>
       {({ data: { me } }) => (
-      <StripeCheckout
-      amount={calcTotalPrice(me.cart)}
-      name="Sick Fits"
-      description={`Order of ${totalItems(me.cart)} items!`}
-      image={me.cart[0].item && me.cart[0].item.image}
-      stripeKey="pk_test_24isWthp2xCIv4z6qqQhc2rZ"
-      currency="USD"
-      email={me.email}
-      token={res => this.onToken(res)}
-      >{this.props.children}</StripeCheckout>)}
+        <Mutation mutation={CREATE_ORDER_MUTATION} refetchQueries={[{ query: CREATE_ORDER_MUTATION }]}>
+          {(createOrder) => (
+            <StripeCheckout
+            amount={calcTotalPrice(me.cart)}
+            name="Sick Fits"
+            description={`Order of ${totalItems(me.cart)} items!`}
+            image={me.cart.length && me.cart[0].item && me.cart[0].item.image}
+            stripeKey="pk_test_24isWthp2xCIv4z6qqQhc2rZ"
+            currency="USD"
+            email={me.email}
+            token={res => this.onToken(res, createOrder)}
+            >{this.props.children}</StripeCheckout>
+          )}
+        </Mutation>
+      )}
       </User>
     )
   }
