73db1f42be82cb42a6f067e74063e4de1112d961
Alex Matseshe <amatseshe@gmail.com>
2018-12-04 16:03:07 -0600

    Add tests

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -9,7 +9,7 @@ import CartCount from './CartCount';
 const Nav = () => (
   <User>
     {({data: { me }}) => (
-      <NavStyles>
+      <NavStyles data-test="nav">
         <Link href="/items">
           <a>Shop</a>
         </Link>
